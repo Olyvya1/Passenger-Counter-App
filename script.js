bbd86ad4c2cac219decd1920d59ddaa32951405b
Olive <ukohaolive@gmail.com>
2022-09-01 00:26:07 +0100

    new commit

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-let count = 5
+let countDisplay = document.getElementById("count-display")
+let entriesLog = document.getElementById("entries-log")
+let count = 0
+
+function increment () {
+    count = count += 1
+    countDisplay.textContent = count
+}
+ function save() {
+    let countStr = count +  "-"
+    entriesLog.textContent += countStr
+    countDisplay.textContent = 0
+    count = 0
+ }
